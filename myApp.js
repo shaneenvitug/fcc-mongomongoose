e1949c5d0b83673cc940d6a0d272ec236bb64781
Shaneen Vitug <shaneenvitug@gmail.com>
2020-05-19 15:24:33 +1000

    install and setup mongoose

--- myApp.js
+++ myApp.js
@@ -13,8 +13,8 @@
 // as MONGO_URI. Connect to the database using the following syntax:
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
-
-
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
